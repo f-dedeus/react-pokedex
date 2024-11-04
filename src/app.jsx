4d0fc630de54f8bae60ccdc3401efb804724c0ee
fdedeus <filipededeus252@gmail.com>
2024-11-04 15:32:06 -0300

    no message

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import SearchBar from './components/searchBar.jsx';
 import PokemonList from './components/pokemonList.jsx';
 import PokemonDetails from './components/pokemonDetails.jsx';
@@ -9,8 +8,8 @@ function App() {
   const [pokemonList, setPokemonList] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [searchQuery, setSearchQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const pokemonPerPage = 20;
+  const [page] = useState(1);
+  const pokemonPerPage = 151;
 
   // Buscar a lista de Pokémon
   useEffect(() => {
@@ -50,24 +49,6 @@ function App() {
         pokemonList={paginatedPokemon}
         onSelectPokemon={(name) => handleSearch(name)}
       />
-
-      {/* Paginação */}
-      <div className="flex justify-center mt-4">
-        <button
-          onClick={() => setPage(page - 1)}
-          disabled={page === 1}
-          className="px-4 py-2 bg-blue-500 text-white rounded mr-2"
-        >
-          Anterior
-        </button>
-        <button
-          onClick={() => setPage(page + 1)}
-          disabled={page === Math.ceil(pokemonList.length / pokemonPerPage)}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Próximo
-        </button>
-      </div>
     </div>
   );
 }
--- src/components/pagination.jsx
+++
@@ -1,7 +0,0 @@
-const [page, setPage] = useState(1);
-const pokemonPerPage = 20;
-
-const paginatedPokemon = pokemonList.slice(
-  (page - 1) * pokemonPerPage, 
-  page * pokemonPerPage
-);
